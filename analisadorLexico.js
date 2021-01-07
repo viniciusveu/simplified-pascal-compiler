90d9e35ecc5c7c17884a10b2f8b7b8acb062ac3f
viniciusveu <vinicius.vedovotto@unesp.br>
2021-01-07 14:30:45 -0300

    Analisador sintático

--- analisadorLexico.js
+++ analisadorLexico.js
@@ -1,4 +1,3 @@
-
 const tipoToken = {
     NUM_INT: "numInt",
     NUM_REAL: "numReal",
@@ -21,7 +20,8 @@ const tipoToken = {
     PALAVRA: "palavraReservada",
     IDENTIFICADOR: "palavraIdentificadora",
     COMENT: "comentario",
-    ERRO: "erro"
+    PT: "pontoFinal",
+    ERRO: "erroLexico"
 }
 
 const tipoErro = {
@@ -32,19 +32,19 @@ const tipoErro = {
 }
 
 const simbolos = [
-    '+', '-', '*', '/', '(', ')', ',', ';', ':', '>', '<', '='
+    '+', '-', '*', '/', '(', ')', ',', ';', ':', '>', '<', '=', "."
 ]
 
-const palavras = [
-    'program', 'procedure', 'var', 'begin', 'end', 'end.', 'if', 'then', 'else', 'while', 'do', 'or', 'div', 'and', 'not', 'int', 'boolean', 'true', 'false', 'read', 'write'
+const palavrasRes = [
+    'program', 'procedure', 'var', 'begin', 'end', 'if', 'then', 'else', 'while', 'do', 'or', 'div', 'and', 'not', 'int', 'boolean', 'true', 'false', 'read', 'write'
 ]
 
 function geraToken(lexema, token, linha, colIni, colFin, erro) {
     //console.log('< '+nome+', '+lexema+' >')
     return {
-        lexema, 
+        lexema,
         token,
-        linha, 
+        linha,
         colIni,
         colFin,
         erro
@@ -56,8 +56,8 @@ function pertenceListaSimbolos(element) {
     return false
 }
 
-function pertenceListaPalavras(element) {
-    if (palavras.indexOf(element) != -1) return true
+function pertenceListaPalavrasRes(element) {
+    if (palavrasRes.indexOf(element) != -1) return true
     return false
 }
 
@@ -66,103 +66,117 @@ function verificaCaracteres(element) {
     return regex.test(element) && isNaN(element[0])
 }
 
-function analizadorLexico(data, logs, tokens) {
+function analisadorLexico(data, tokens, linhas) {
 
     let buffer = []
     let bufferStr = ''
     let linha = 1
     let coluna = 0
     let eComentario = false
-  
+    let eComentarioLinha = false
+    linhas.push('1  | ')
 
-    data.forEach( (element, index) => {
+    data.forEach((element, index) => {
+        linhas.push(element)
         coluna++
-        if(element != '\n' || element != ' ' || element != '\t') { //Estado de partida
+        if (element != '\n' || element != ' ' || element != '\t') { //Estado de partida
 
-            if(element != '\n' && element != ' ' && element != '\t' && !pertenceListaSimbolos(element) || eComentario) {
-                if(element == '{' || element == '}') {
+            if (element != '\n' && element != ' ' && element != '\t' && !pertenceListaSimbolos(element) || eComentario || eComentarioLinha) {
+
+                if (element == '{' || element == '}') {
                     element == '{' ? eComentario = true : eComentario = false
+                }
+                if (eComentarioLinha) {
+                    if (element == '\n') {
+                        eComentarioLinha = false
+                        linhas.push(linha + 1 + '  | ')
+                    }
                 }
                 buffer.push(element) // Caso possa ser um numero (numeros e '.')
                 coluna--
-            }
-                
-            else {
+            } else {
                 if (buffer.length) {
                     bufferStr = buffer.join('')
-                    if (buffer[0] == '{') {
-                        tokens.push(geraToken(bufferStr, tipoToken.COMENT, linha, coluna, coluna+buffer.length-1, ' - '))
-                    }
-                    else if (buffer.length > 15) {
-                        tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna+buffer.length-1, tipoErro.OVERFLOW))
-                    } 
-                    else if(buffer.indexOf('.') == -1) {
-                        if(!isNaN(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.NUM_INT, linha, coluna, coluna+buffer.length-1, ' - '))
+                    if (buffer[0] == '{' || buffer[0] == '/') {
+                        tokens.push(geraToken(bufferStr, tipoToken.COMENT, linha, coluna, coluna + buffer.length - 1, ' - '))
+                        coluna = 1
+                        buffer.length = 0
+                    } else if (buffer.length > 15) {
+                        tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna + buffer.length - 1, tipoErro.OVERFLOW))
+                    } else if (buffer.indexOf('.') == -1) {
+                        if (!isNaN(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.NUM_INT, linha, coluna, coluna + buffer.length - 1, ' - '))
                         else {
-                            if(pertenceListaPalavras(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.PALAVRA, linha, coluna, coluna+buffer.length-1, ' - '))
+                            if (pertenceListaPalavrasRes(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.PALAVRA, linha, coluna, coluna + buffer.length - 1, ' - '))
                             else {
-                                if(verificaCaracteres(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.IDENTIFICADOR, linha, coluna, coluna+buffer.length-1, ' - '))
-                                else {
-                                    logs.push("ERRO: "+bufferStr+" número mal formatado!")
-                                    tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna+buffer.length-1, tipoErro.SIMB_DESCONHECIDO))
-                                }
+                                if (verificaCaracteres(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.IDENTIFICADOR, linha, coluna, coluna + buffer.length - 1, ' - '))
+                                else
+                                    tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna + buffer.length - 1, tipoErro.SIMB_DESCONHECIDO))
+
                             }
                         }
                     } else {
-                        if(!isNaN(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.NUM_REAL, linha, coluna, coluna+buffer.length-1, ' - '))
-                        else {
-                            logs.push("ERRO: "+bufferStr+" número mal formatado!")
-                            tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna+buffer.length-1, tipoErro.NUM_MAL_FORMATADO))
-                        }
+                        if (!isNaN(bufferStr)) tokens.push(geraToken(bufferStr, tipoToken.NUM_REAL, linha, coluna, coluna + buffer.length - 1, ' - '))
+                        else
+                            tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna + buffer.length - 1, tipoErro.NUM_MAL_FORMATADO))
                     }
-                    coluna = coluna+buffer.length
+                    coluna = coluna + buffer.length
                     buffer.length = 0
                 }
-                if(element == '+') tokens.push(geraToken(element, tipoToken.OP_SOMA, linha, coluna, coluna, ' - '))
-                else if(element == '-') tokens.push(geraToken(element, tipoToken.OP_SUB, linha, coluna, coluna, ' - '))
-                else if(element == '/') tokens.push(geraToken(element, tipoToken.OP_DIV, linha, coluna, coluna, ' - '))
-                else if(element == '*') tokens.push(geraToken(element, tipoToken.OP_MULT, linha, coluna, coluna, ' - '))
-                else if(element == '(') tokens.push(geraToken(element, tipoToken.ABRE_PAR, linha, coluna, coluna, ' - '))
-                else if(element == ')') tokens.push(geraToken(element, tipoToken.FECHA_PAR, linha, coluna, coluna, ' - '))
-                else if(element == ',') tokens.push(geraToken(element, tipoToken.VIRGULA, linha, coluna, coluna, ' - '))
-                else if(element == ';') tokens.push(geraToken(element, tipoToken.PT_VIRG, linha, coluna, coluna, ' - '))
-                else if(element == ':') {
-                    if (data[index+1] != '=')
+                if (element == '+') tokens.push(geraToken(element, tipoToken.OP_SOMA, linha, coluna, coluna, ' - '))
+                else if (element == '-') tokens.push(geraToken(element, tipoToken.OP_SUB, linha, coluna, coluna, ' - '))
+                else if (element == '*') tokens.push(geraToken(element, tipoToken.OP_MULT, linha, coluna, coluna, ' - '))
+                else if (element == '(') tokens.push(geraToken(element, tipoToken.ABRE_PAR, linha, coluna, coluna, ' - '))
+                else if (element == ')') tokens.push(geraToken(element, tipoToken.FECHA_PAR, linha, coluna, coluna, ' - '))
+                else if (element == ',') tokens.push(geraToken(element, tipoToken.VIRGULA, linha, coluna, coluna, ' - '))
+                else if (element == ';') tokens.push(geraToken(element, tipoToken.PT_VIRG, linha, coluna, coluna, ' - '))
+                else if (element == '.') tokens.push(geraToken(element, tipoToken.PT, linha, coluna, coluna, ' - '))
+                else if (element == '/') {
+                    if (data[index + 1] != '/' && data[index - 1] != '/') tokens.push(geraToken(element, tipoToken.OP_DIV, linha, coluna, coluna, ' - '))
+                    else {
+                        eComentarioLinha = true
+                        buffer.push(element)
+                        coluna--
+                    }
+                } else if (element == ':') {
+                    if (data[index + 1] != '=')
                         tokens.push(geraToken(element, tipoToken.DOIS_PTS, linha, coluna, coluna, ' - '))
-                }
-                else if (element == '<') {
-                    if (data[index+1] != '=' && data[index+1] != '>')
+                } else if (element == '<') {
+                    if (data[index + 1] != '=' && data[index + 1] != '>')
                         tokens.push(geraToken(element, tipoToken.MENOR_QUE, linha, coluna, coluna, ' - '))
-                }    
-                else if (element == '>') {
-                    if (data[index+1] != '=' && data[index-1] != '<')
+                } else if (element == '>') {
+                    if (data[index + 1] != '=' && data[index - 1] != '<')
                         tokens.push(geraToken(element, tipoToken.MAIOR_QUE, linha, coluna, coluna, ' - '))
-                    else if (data[index-1] == '<') tokens.push(geraToken(data[index-1]+element, tipoToken.DIFERENTE, linha, coluna-1, coluna, ' - '))
-                }
-                else if (element == '='){
-                    if (data[index-1] == ':')   
-                        tokens.push(geraToken(data[index-1]+element, tipoToken.ATRIBUICAO, linha, coluna-1, coluna, ' - '))
-                    else if (data[index-1] == '>')   
-                        tokens.push(geraToken(data[index-1]+element, tipoToken.MAIOR_IGUAL, linha, coluna-1, coluna, ' - '))
-                    else if (data[index-1] == '<')   
-                        tokens.push(geraToken(data[index-1]+element, tipoToken.MENOR_IGUAL, linha, coluna-1, coluna, ' - '))
+                    else if (data[index - 1] == '<') tokens.push(geraToken(data[index - 1] + element, tipoToken.DIFERENTE, linha, coluna - 1, coluna, ' - '))
+                } else if (element == '=') {
+                    if (data[index - 1] == ':')
+                        tokens.push(geraToken(data[index - 1] + element, tipoToken.ATRIBUICAO, linha, coluna - 1, coluna, ' - '))
+                    else if (data[index - 1] == '>')
+                        tokens.push(geraToken(data[index - 1] + element, tipoToken.MAIOR_IGUAL, linha, coluna - 1, coluna, ' - '))
+                    else if (data[index - 1] == '<')
+                        tokens.push(geraToken(data[index - 1] + element, tipoToken.MENOR_IGUAL, linha, coluna - 1, coluna, ' - '))
                     else tokens.push(geraToken(element, tipoToken.IGUAL, linha, coluna, coluna, ' - '))
                 }
-                
-                if(element == '\n') {
+
+                if (element == '\n') {
                     linha++
                     coluna = 0
+                    linhas.push(linha + '  | ')
                 }
                 if (element == '\t') coluna = coluna + 3
             }
         }
+
     });
     if (eComentario) {
         bufferStr = buffer.join('')
-        tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna,  coluna+buffer.length-1, tipoErro.COMENT_NAO_FECHADO))
+        tokens.push(geraToken(bufferStr, tipoToken.ERRO, linha, coluna, coluna + buffer.length - 1, tipoErro.COMENT_NAO_FECHADO))
     }
+
+
 }
 
 
-module.exports = analizadorLexico
-
+module.exports = {
+    analisadorLexico,
+    tipoToken
+}
